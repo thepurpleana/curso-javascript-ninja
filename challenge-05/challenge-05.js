93bd3c35e9247936d22f44105bee68ec435c03ea
thepurpleana <thepurpleana@gmail.com>
2018-07-24 17:21:29 -0300

    challenge-5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,17 +3,19 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
-
+var myArray = [10, 20, 34, 32, 3];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
-
+function getArray(a) {
+    return a; 
+}
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
-
+    var newArray = getArray(myArray)[1];
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -21,17 +23,31 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
-
+function getAtIndex(arr, i) {
+    if (arr.length < i) {
+    return arr[i];
+    }
+    return "Not possible, index out of array";
+}
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
+var myObject = {
+    isEmpty: false,
+    content: "nothing"
+}
 
+var messArray = [2, "hello", false, getAtIndex(), myObject];
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+
+for (var i=0; i < messArray.length; i++) {
+    getAtIndex(messArray, i);
+}
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -48,6 +64,18 @@ propriedades:
 os livros.
 */
 // ?
+
+function book(name) {
+    var library = {
+        bookName: {
+            quantidadePaginas: 200,
+            autor: "Euzinha",
+            editora: "Intrinseca"
+        };
+    };
+
+    return "que";
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
