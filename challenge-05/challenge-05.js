8b33691e51e0b25ba5c782a58e28830fc7a4be36
thepurpleana <thepurpleana@gmail.com>
2018-07-25 19:49:53 -0300

    why

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,7 +3,7 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
-var myArray = [10, 20, 34, 32, 3];
+var myArray = [10, 20, 34, 34, 3];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
@@ -15,7 +15,7 @@ function getArray(a) {
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
-    var newArray = getArray(myArray)[1];
+   console.log( getArray(myArray)[1]);
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -35,7 +35,7 @@ var myObject = {
     content: "nothing"
 }
 
-var messArray = [2, "hello", false, getAtIndex(), myObject];
+var messArray = [2, "hello", false, {b: 34}, myObject];
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
@@ -43,7 +43,7 @@ array criado.
 // ?
 
 for (var i=0; i < messArray.length; i++) {
-    getAtIndex(messArray, i);
+    console.log(getAtIndex(messArray, i));
 }
 
 /*
@@ -62,40 +62,53 @@ os livros.
 */
 // ?
 
-function book(name) {
+function searchBook(name) {
     var library = {
-        bookName: {
+        'bookName': {
             quantidadePaginas: 200,
             autor: "Euzinha",
             editora: "Intrinseca"
-        }
+        },
+        'bookName2': {
+            quantidadePaginas: 200,
+            autor: "Euzinha",
+            editora: "Intrinseca"
+        },
+        'bookName3': {
+            quantidadePaginas: 200,
+            autor: "Euzinha",
+            editora: "Intrinseca"
+        },
     };
 
-    return "que";
+    return !name ? library : library[name];
 }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
-Hello
+console.log(searchBook());
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
 // ?
-
+var book = 'bookName';
+var selected = searchBook(book);
+console.log("O livro " + book + " tem " + selected.quantidadePaginas + " páginas!");
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
 // ?
-
+console.log("O autor do livro " + book + " é " + selected.autor + ".");
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
 // ?
+console.log("O livro " + book + " foi publicado pela editora " + selected.editora + ".");
